68486475b9c21fc33bec31a9f601ff1b304501c0
João Victor Santos <68966095+JoaoV-Santos@users.noreply.github.com>
2024-03-24 10:56:11 -0300

    Questao10.java

--- Questao10.java
+++ Questao10.java
@@ -4,10 +4,18 @@ public class Questao10 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
 
+        int c = 0;
+
         System.out.print("Informe o primeiro valor: ");
         int a = in.nextInt();
         System.out.print("Informe o segundo valor: ");
         int b = in.nextInt();
+
+        if (b < a){
+            c = a;
+            a = b;
+            b = c;
+        }
 
         for (int i = a; i < b; i++)
             System.out.println(i);
